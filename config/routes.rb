a1653e7dd92751cfdbc6dde1b785f5a34b3d0767
Christianchesire <christian.chesire@strathmore.edu>
2021-04-14 00:36:40 -0700

    Make app production ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index]
+  resources :articles
+  # Above has same effect 
+  #, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  # resources provides REST-ful routes to Rails resources
 end
