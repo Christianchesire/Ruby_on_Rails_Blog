2bbe95e894560d47652084a1ed80e256ce11e8cc
Christianchesire <christian.chesire@strathmore.edu>
2021-05-07 06:20:56 -0700

    Config assets initialize false

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,10 @@ module AlphaBlog
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
+
+    #config/application.rb
+    config.assets.Initialize_on_precompile = false
+
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
