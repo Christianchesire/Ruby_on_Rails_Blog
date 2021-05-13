541b48735b22a0625c91e8e5aaeeadfb41914b4c
Christianchesire <christian.chesire@strathmore.edu>
2021-05-13 01:24:04 -0700

    Sign-up page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # Above has same effect 
   #, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   # resources provides REST-ful routes to Rails resources
