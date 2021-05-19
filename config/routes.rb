9f3d6d847a60a65070ea8d64e1fee57bd0a80a3e
Christianchesire <christian.chesire@strathmore.edu>
2021-05-19 02:07:30 -0700

    Adding Login form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # Above has same effect 
   #, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   # resources provides REST-ful routes to Rails resources
