29d0123dbf9fdd2d3da4c17abb4c86d0a24b3875
Christianchesire <christian.chesire@strathmore.edu>
2021-05-25 04:05:08 -0700

    Added access restrictions on UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user, :logged_in?
+    def current_user
+        #use  memoization for user reference
+        @current_user ||=User.find(session[:user_id]) if session[:user_id]
+    end 
+
+    def logged_in?
+        # Turn current user into boolean , to check if exist i.e  1=True or 0=False
+        !!current_user
+    end
+
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
     def create
         # creates new article and saves them.
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         if  @article.save
             flash[:notice] = "Article was created successfully."
             # redirect our saved article to article_path for display
