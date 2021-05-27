7146548d43bdb36882fb43ab55a74f458df1e3bc
Christianchesire <christian.chesire@strathmore.edu>
2021-05-27 02:05:27 -0700

    Adding delete user function

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :articles
   get 'signup', to: 'users#new'
   
-  resources :users, except: [:new]
+  resources :users, except: [:new] # Gives us all restful routes for users
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
