4063f82971f206a1af6d6b2c5a531bd1c4a9991f
Christianchesire <christian.chesire@strathmore.edu>
2021-05-27 00:37:37 -0700

    Added restrict actions for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update]
+    before_action :require_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update]
 
     def show 
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -54,4 +56,12 @@ class UsersController < ApplicationController
     def article_params
         params.require(:article).permit(:title, :description)
     end
+
+    def require_same_user
+        if current_user != @user
+            flash[:alert] = "You can only edit your own account"
+            redirect_to @user
+        end
+    end
+
 end 
